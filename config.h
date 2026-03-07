6125371e1edb29d919940269d2707aa54a81f589
Konrad Gasch <gasch.communication@tuta.io>
2026-03-07 18:47:58 +0100

    updated dwl config.h

--- config.h
+++ config.h
@@ -6,10 +6,10 @@
 /* appearance */
 static const int sloppyfocus               = 1;  /* focus follows mouse */
 static const int bypass_surface_visibility = 0;  /* 1 means idle inhibitors will disable idle tracking even if it's surface isn't visible  */
-static const unsigned int borderpx         = 1;  /* border pixel of windows */
+static const unsigned int borderpx         = 2;  /* border pixel of windows */
 static const int showbar                   = 1; /* 0 means no bar */
 static const int topbar                    = 1; /* 0 means bottom bar */
-static const char *fonts[]                 = {"monospace:size=10"};
+static const char *fonts[]                 = {"monospace:size=12"};
 static const float rootcolor[]             = COLOR(0x000000ff);
 static const unsigned int gappih           = 10; /* horiz inner gap between windows */
 static const unsigned int gappiv           = 10; /* vert inner gap between windows */
@@ -21,8 +21,8 @@ static const int monoclegaps               = 0;  /* 1 means outer gaps in monocl
 static const float fullscreen_bg[]         = {0.0f, 0.0f, 0.0f, 1.0f}; /* You can also use glsl colors */
 static uint32_t colors[][3]                = {
 	/*               fg          bg          border    */
-	[SchemeNorm] = { 0xbbbbbbff, 0x222222ff, 0x444444ff },
-	[SchemeSel]  = { 0xeeeeeeff, 0x005577ff, 0x005577ff },
+	[SchemeNorm] = { 0xeeeeeeff, 0x101010cc, 0xeeeeeeff },
+	[SchemeSel]  = { 0xeeeeeeff, 0x101010cc, 0xb8bb26ff },
 	[SchemeUrg]  = { 0,          0,          0x770000ff },
 };
 
@@ -132,9 +132,27 @@ static const enum libinput_config_tap_button_map button_map = LIBINPUT_CONFIG_TA
 static const char *termcmd[] = { "foot", NULL };
 static const char *menucmd[] = { "wmenu-run", NULL };
 
+static const char *lockcmd[]        = { "swaylock", NULL };
+static const char *vol_upcmd[]      = { "pulsemixer", "--set-volume", "+10", NULL };
+static const char *vol_downcmd[]      = { "pulsemixer", "--set-volume", "-10", NULL };
+static const char *vol_mutecmd[]      = { "pulsemixer", "--mute", NULL };
+static const char *powercmd[]       = { "/home/konrad/.config/dwl/scripts/powermenu.sh", NULL };
+static const char *mon_cmd[]        = { "/home/konrad/.config/dwl/scripts/monitor.sh", NULL };
+static const char *bright_upcmd[]   = { "brightnessctl", "-d", "*", "-c", "backlight", "set", "+10%", NULL };
+static const char *bright_downcmd[] = { "brightnessctl", "-d", "*", "-c", "backlight", "set", "10%-", NULL };
+
 static const Key keys[] = {
 	/* Note that Shift changes certain key codes: 2 -> at, etc. */
 	/* modifier                  key                  function          argument */
+    { 0,                         XKB_KEY_XF86MonBrightnessDown, 	spawn,          {.v = bright_downcmd} },
+    { 0,                         XKB_KEY_XF86MonBrightnessUp, 	spawn,          {.v = bright_upcmd} },
+    { 0,                         XKB_KEY_XF86Sleep,               spawn,          {.v = lockcmd} },
+    { 0,                         XKB_KEY_F7,                      spawn,          {.v = mon_cmd} },
+    { 0,                         XKB_KEY_XF86ScreenSaver,         spawn,          {.v = lockcmd} },
+    { 0,                         XKB_KEY_XF86AudioMute,           spawn,          {.v = vol_mutecmd} },
+    { 0,                         XKB_KEY_XF86AudioLowerVolume,    spawn,          {.v = vol_downcmd} },
+    { 0,                         XKB_KEY_XF86AudioRaiseVolume,    spawn,          {.v = vol_upcmd} },
+    { MODKEY,                    XKB_KEY_Escape,                  spawn,          {.v = powercmd} },
 	{ MODKEY,                    XKB_KEY_p,           spawn,            {.v = menucmd} },
 	{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_Return,      spawn,            {.v = termcmd} },
 	{ MODKEY|WLR_MODIFIER_LOGO,  XKB_KEY_h,          incgaps,       {.i = +1 } },
